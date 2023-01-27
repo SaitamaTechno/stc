1802961ded1fab8fcccd786ee169a83763307a6d
SaitamaTechno <95487121+SaitamaTechno@users.noreply.github.com>
2023-01-27 14:56:56 +0300

    Update stc1.sh

--- stc1.sh
+++ stc1.sh
@@ -5,16 +5,6 @@ apt install expect -y
 apt install ssh -y
 apt install net-tools -y
 
-/usr/bin/expect <<EOD
-spawn passwd
-expect "New password:"
-send -- "123456\r"
-expect "Retype new password:"
-send -- "123456\r"
-interact
-EOD
-echo "you're out"
-
 #nano /etc/ssh/sshd_config
 
 echo "Include /etc/ssh/sshd_config.d/*.conf
@@ -26,4 +16,4 @@ PrintMotd no
 AcceptEnv LANG LC_*
 Subsystem       sftp    /usr/lib/openssh/sftp-server" > /etc/ssh/sshd_config
 
-service ssh restart
+#service ssh restart
